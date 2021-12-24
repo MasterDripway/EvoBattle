50854164eca56bd7563b9e464d07260b6bb39847
Master Dripway <manuelinegron@gmail.com>
2021-12-24 11:34:26 -0500

    added custom class import

--- consolehost.py
+++ consolehost.py
@@ -6,9 +6,9 @@ import importlib
 
 
 def main():
-    p1 = "tank"
+    p1 = input("Player 1, what is your name? ")
     ch1 = getattr(importlib.import_module("assets." + p1), p1)()
-    p2 = "dummy"
+    p2 = input("Player 2, what is your name? ")
     ch2 = getattr(importlib.import_module("assets." + p2), p2)()
 
     battle(ch1, ch2).start()
