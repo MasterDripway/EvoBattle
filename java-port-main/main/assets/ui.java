6e42307833069bfb847c273ea1b8322b5b900f4f
Master Dripway <manuelinegron@gmail.com>
2022-01-03 17:31:09 -0500

    last ui fix today

--- java-port-main/main/assets/ui.java
+++ java-port-main/main/assets/ui.java
@@ -9,21 +9,16 @@ public class ui extends JFrame {
 
     public ui() {
         MainPanel mp = new MainPanel();
-        setLayout(null);
-        mp.setBounds(401,0,400,400);
+        setLayout(new BorderLayout());
 
-        BoxPanel bp = new BoxPanel(BoxLayout.Y_AXIS);
-        //bp.setBorder(50, 50, 50, 50);
-        bp.add(new ClassicButton("Click me"));
-        bp.setBounds(0, 0, 400, 400);
-        add(bp);
-        add(mp);
+        mp.setBounds(0, 0, 400, 400);
+        add(mp, BorderLayout.CENTER);
         setTitle("Match");
         getContentPane().setBackground(Color.red);
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
         setSize(800, 800);
-        //pack();
         setVisible(true);
         
     }
